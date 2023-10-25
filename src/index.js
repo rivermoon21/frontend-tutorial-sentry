f47de23be0c67aed2ee09100c6063cbf44247cab
Mauricio <mrivermoon@protonmail.com>
2023-10-24 20:06:01 -0700

    frontend

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./components/App";
+
+// Import and init Sentry SDK
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://9b1155a54c47cf18ab599306d2ffd4c8@o4506063753969664.ingest.sentry.io/4506108477177856",
+  integrations: [new Sentry.BrowserTracing(), new Sentry.Replay()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  tracesSampleRate: 1.0,
+
+  // Capture Replay for 10% of all sessions,
+  // plus for 100% of sessions with an error
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0,
+});
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(<App />);
